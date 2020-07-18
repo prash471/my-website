3e470c82f9dc5c1b97e1982ba81e52ac92b8002a
Prashant Singh <prash471@gmail.com>
2020-07-19 01:27:52 +0800

    Updates

---
+++ precache-manifest.5d39d1d269a15c5c08ff7f003d2f8bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b42cd571eaa93415818",
+    "url": "/my-website/static/css/main.84ccf359.chunk.css"
+  },
+  {
+    "revision": "5b42cd571eaa93415818",
+    "url": "/my-website/static/js/main.5b42cd57.chunk.js"
+  },
+  {
+    "revision": "3819f5abdca58cb6c031",
+    "url": "/my-website/static/js/runtime~main.3819f5ab.js"
+  },
+  {
+    "revision": "cac89f78aa57a7c4346b",
+    "url": "/my-website/static/js/2.cac89f78.chunk.js"
+  },
+  {
+    "revision": "95ab144bc299d5d9e2817d26bb964a8f",
+    "url": "/my-website/index.html"
+  }
+];
