5ada57ad70dd5eebd816f26c9ec06f451d055bdf
Prashant Singh <prash471@gmail.com>
2020-07-18 22:43:48 +0800

    updated files

---
+++ build/precache-manifest.1822194ef91d8932bdfd542a787aae0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "367beb65797570ee890c",
+    "url": "/static/css/main.84ccf359.chunk.css"
+  },
+  {
+    "revision": "367beb65797570ee890c",
+    "url": "/static/js/main.367beb65.chunk.js"
+  },
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "cac89f78aa57a7c4346b",
+    "url": "/static/js/2.cac89f78.chunk.js"
+  },
+  {
+    "revision": "45f6dc15871926a4eec733c4e18ac5b7",
+    "url": "/index.html"
+  }
+];
